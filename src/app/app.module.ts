4a07889eb265b6c397c627e163f53726b5e45c8a
unknown <you@example.com>
2020-09-29 10:59:42 +0200

    TP collegues front routeur menu ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { AppComponent } from './app.component';
 import { CollegueComponent } from './collegue/collegue.component';
 import { RechercheCollegueParNomComponent } from './recherche-collegue-par-nom/recherche-collegue-par-nom.component';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
 import { CreerCollegueComponent } from './creer-collegue/creer-collegue.component';
+import { FormsModule } from '@angular/forms';
+import { MenuComponentComponent } from './menu-component/menu-component.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PageAccueilComponent } from './page-accueil/page-accueil.component';
+import { PageGalerieComponent } from './page-galerie/page-galerie.component';
+import { PageAproposComponent } from './page-apropos/page-apropos.component';
+import { PageCreerCollegueComponent } from './page-creer-collegue/page-creer-collegue.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
+  { path: 'accueil', component: PageAccueilComponent },
+  { path: 'galerie', component: PageGalerieComponent },
+  { path: 'apropos', component: PageAproposComponent },
+  { path: 'creer', component: PageCreerCollegueComponent },
+  { path: '', pathMatch: 'full', redirectTo: '/accueil' }
+];
 
 @NgModule({
   declarations: [
@@ -14,17 +29,21 @@ import { CreerCollegueComponent } from './creer-collegue/creer-collegue.componen
     CollegueComponent,
     RechercheCollegueParNomComponent,
     CreerCollegueComponent,
-    
+    MenuComponentComponent,
+    PageAccueilComponent,
+    PageGalerieComponent,
+    PageAproposComponent,
+    PageCreerCollegueComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
-    
+    FormsModule,
+    NgbModule,
+    RouterModule.forRoot(routes)
   ],
-
   providers: [],
   bootstrap: [AppComponent]
 })
-
 export class AppModule { }
+
